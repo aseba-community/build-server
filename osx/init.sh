955e38ab455c2b79ff32445136a9ab1372a2828f
Martin Voelkle <martin.voelkle@epfl.ch>
2015-08-24 09:37:06 +0200

    auto VM startup, automatic login

--- osx/init.sh
+++ osx/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+USER=administrator
+
 # enable ssh login
 cp authorized_keys ~/.ssh/authorized_keys
 launchctl load -w /System/Library/LaunchDaemons/ssh.plist
@@ -14,6 +16,7 @@ scutil --set HostName "$HOSTNAME"
 scutil --set LocalHostName "$HOSTNAME"
 defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$HOSTNAME"
 
+# install VirtualBox
 if [ ! -f VirtualBox.dmg ]
 then curl --output VirtualBox.dmg http://download.virtualbox.org/virtualbox/5.0.2/VirtualBox-5.0.2-102096-OSX.dmg
 fi
@@ -21,6 +24,7 @@ hdiutil attach VirtualBox.dmg
 installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
 hdiutil detach /Volumes/VirtualBox
 
+# install JDK
 if [ ! -f jdk-macosx-x64.dmg ]
 then curl --cookie oraclelicense=a --location --output jdk-macosx-x64.dmg http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-macosx-x64.dmg
 fi
@@ -28,21 +32,22 @@ hdiutil attach jdk-macosx-x64.dmg
 installer -package "/Volumes/JDK 8 Update 60/JDK 8 Update 60.pkg" -target /
 hdiutil detach "/Volumes/JDK 8 Update 60"
 
-mkdir -p /etc/vbox
-cat > /etc/vbox/autostart.cfg << EOF
-default_policy = deny
-administrator = {
-	allow = true
-}
-EOF
-cp /Applications/VirtualBox.app/Contents/MacOS/org.virtualbox.vboxautostart.plist /Library/LaunchDaemons/
-launchctl load -w /Library/LaunchDaemons/org.virtualbox.vboxautostart.plist
-
-sudo -i -u administrator << EOF
+# setup user
+sudo -i -u $USER << EOF
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 VBoxManage registervm build-linux/build-linux.vbox
-VBoxManage modifyvm build-linux --autostart-enabled on
+mkdir -p Library/LaunchAgents
 EOF
 
+# setup login items
+cp *.plist "/Users/$USER/Library/LaunchAgents/"
+chown "$USER:staff" /Users/$USER/Library/LaunchAgents/*.plist
+
+# login automatically
+defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser $USER
+
+# install updates
 softwareupdate --install --all
 
 reboot
