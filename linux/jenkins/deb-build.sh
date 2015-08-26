a80b374e44a09051a338143ef0b78f114d0c0cbe
Martin Voelkle <martin.voelkle@epfl.ch>
2015-08-26 15:02:55 +0200

    use git-buildpackage

--- linux/jenkins/deb-build.sh
+++ linux/jenkins/deb-build.sh
@@ -5,4 +5,4 @@ WORKSPACE=$1
 cd "$WORKSPACE/source"
 #TODO: make apt-get check signatures to get rid of --force-yes
 mk-build-deps --install --tool "apt-get --no-install-recommends --assume-yes --force-yes" --remove
-dpkg-buildpackage -us -uc
+git-buildpackage -us -uc
