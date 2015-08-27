2441b191a94e2398298dd201b809d9c38d507bc8
Martin Voelkle <martin.voelkle@epfl.ch>
2015-08-27 10:34:39 +0200

    clean after build

--- linux/jenkins/deb-build.sh
+++ linux/jenkins/deb-build.sh
@@ -6,4 +6,4 @@ WORKSPACE=$1
 cd "$WORKSPACE/source"
 #TODO: make apt-get check signatures to get rid of --force-yes
 mk-build-deps --install --tool "apt-get --no-install-recommends --assume-yes --force-yes" --remove
-git-buildpackage -us -uc
+git-buildpackage -us -uc -tc
