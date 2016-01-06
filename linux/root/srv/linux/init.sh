eeb3383c03adf46fc8599a897901838c6d79a0bb
Martin Voelkle <martin.voelkle@epfl.ch>
2016-01-06 16:55:14 +0100

    set sudoers file permission

--- linux/root/srv/linux/init.sh
+++ linux/root/srv/linux/init.sh
@@ -16,6 +16,7 @@ yum --assumeyes install git debootstrap dpkg-dev lftp
 curl --output /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum --assumeyes install java jenkins
+chmod ug=r,o= /etc/sudoers.d/jenkins
 chown --recursive jenkins:jenkins /var/lib/jenkins
 systemctl enable jenkins.service
 
